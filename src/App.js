81e627a7c3b070dca2b94316c1c10170fe3847b3
Diego França <diego.franca92@gmail.com>
2021-04-29 06:41:51 -0300

    aplicação da lingua pt-BR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { i18n } from './translate/i18n'
+// https://www.youtube.com/watch?v=5p9EMWwieyo
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{i18n.t('titles.app')}</h1>
+      <p>{i18n.t('messages.itWorks')}</p>
+      <button>{i18n.t('buttons.save')}</button>
+      <small>{i18n.t('messages.smallText')}</small>
     </div>
   );
 }
---
+++ src/translate/languages/pt.js
@@ -0,0 +1,16 @@
+const messages = {
+    translations: {
+        titles: {
+            app: 'Minha aplicação'
+        },
+        messages: {
+            itWorks: 'Está funcionado belezinha',
+            smallText: 'Um pequeno texto'
+        },
+        buttons: {
+            save: 'Salvar'
+        }
+    }
+}
+
+export { messages }
